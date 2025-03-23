49c0a65eb12d4ef80b825b1560bbe961945b0ade
Sandra <kamdzijas.sandra@gmail.com>
2025-03-23 21:19:07 +0100

    Cosmetic update

--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -4,7 +4,7 @@ const ErrorMessages = require('../pageobjects/errorMessages.js'); // Import Erro
 const credentials = require('../pageobjects/loginCredentials.js'); // Import login credentials
 //#endregion 
 
-describe.skip('Swag Labs - Log In Page', () => {  
+describe('Swag Labs - Log In Page', () => {  
 
     beforeEach(async () => { 
         // Open the login page before each test
@@ -12,7 +12,7 @@ describe.skip('Swag Labs - Log In Page', () => {
     });
 
     it('should log in successfully with valid credentials', async () => { 
-        // Use valid credentials from loginCredentials.js
+        // Log in with valid credentials
         await LoginPage.login(credentials.validUsername, credentials.validPassword);
         
         // Expected result: User should be redirected to the inventory page with the URL containing 'inventory.html'
@@ -24,13 +24,13 @@ describe.skip('Swag Labs - Log In Page', () => {
         // Try to log in with an invalid username and valid password from loginCredentials.js
         await LoginPage.login(credentials.invalidValue, credentials.validPassword); 
         
-        // Expected result: Error message indicating that the username and password do not match any user in the service
+        // Expected result: Error message indicating that the username and password do not match any user in the service 
         const errorMessage = await LoginPage.getErrorMessage(); 
         await expect(errorMessage).toContain(ErrorMessages.usernameAndPasswordDoNotMatch);
     }); 
     
     it('should display an error message when an invalid password is provided', async () => {
-        // Try to log in with a valid username and an invalid password from loginCredentials.js
+        // Try to log in with a valid username and an invalid password 
         await LoginPage.login(credentials.validUsername, credentials.invalidValue);  
         
         // Expected result: Error message indicating that the username and password do not match any user in the service
@@ -39,16 +39,16 @@ describe.skip('Swag Labs - Log In Page', () => {
     }); 
     
     it('should display an error message when the username field is empty', async () => {
-        // Try to log in without providing a username, but with a valid password from loginCredentials.js
+        // Try to log in without providing a username, but with a valid password 
         await LoginPage.login(credentials.emptyValue, credentials.validPassword); 
-        
+
         // Expected result: Error message indicating that the username field is required
         const errorMessage = await LoginPage.getErrorMessage();
         await expect(errorMessage).toContain(ErrorMessages.usernameRequired);
     }); 
     
     it('should display an error message when the password field is empty', async () => {
-        // Try to log in with a valid username, but without providing a password from loginCredentials.js
+        // Try to log in with a valid username, but without providing a password
         await LoginPage.login(credentials.validUsername, credentials.emptyValue); 
         
         // Expected result: Error message indicating that the password field is required
@@ -57,7 +57,7 @@ describe.skip('Swag Labs - Log In Page', () => {
     }); 
     
     it('should display an error message when both username and password fields are empty', async () => {
-        // Try to log in without providing both a username and a password from loginCredentials.js
+        // Try to log in without providing both a username and a password 
         await LoginPage.login(credentials.emptyValue, credentials.emptyValue); 
         
         // Expected result: Error message indicating that the username is required
@@ -65,8 +65,8 @@ describe.skip('Swag Labs - Log In Page', () => {
         await expect(errorMessage).toContain(ErrorMessages.usernameRequired);
     });  
     
-    it('should display an error message when invalid username and valid password are provided (case-sensitive)', async () => {
-        // Try to log in with an invalid username and a valid password (case-sensitive) from loginCredentials.js
+    it('should display an error message when invalid (case-sensitive) username and valid password are provided', async () => {
+        // Try to log in with an invalid username (case-sensitive) and a valid password 
         await LoginPage.login(credentials.invalidUsernameCaseSensitive, credentials.validPassword);  
         
         // Expected result: Error message indicating that the username and password do not match any user in the service
@@ -74,14 +74,23 @@ describe.skip('Swag Labs - Log In Page', () => {
         await expect(errorMessage).toContain(ErrorMessages.usernameAndPasswordDoNotMatch);
     });
     
-    it('should display an error message when valid username and invalid password are provided (case-sensitive)', async () => {
-        // Try to log in with a valid username and an invalid password (case-sensitive) from loginCredentials.js
+    it('should display an error message when valid username and invalid (case-sensitive) password are provided', async () => {
+        // Try to log in with a valid username and an invalid (case-sensitive) password
         await LoginPage.login(credentials.validUsername, credentials.invalidPasswordCaseSensitive); 
         
         // Expected result: Error message indicating that the username and password do not match any user in the service
         const errorMessage = await LoginPage.getErrorMessage();
         await expect(errorMessage).toContain(ErrorMessages.usernameAndPasswordDoNotMatch);
-    });  
+    });   
+     
+    it('should display an error message when invalid credentials are provided', async () => {
+        // Try to log in with invalid credentials
+        await LoginPage.login(credentials.invalidValue, credentials.invalidValue); 
+        
+        // Expected result: Error message indicating that the username and password do not match any user in the service
+        const errorMessage = await LoginPage.getErrorMessage();
+        await expect(errorMessage).toContain(ErrorMessages.usernameAndPasswordDoNotMatch);
+    });
 
 });
 
